5d9a01e2625d741df4a17538c05b8fcac1580064
tahiraliprofile <tahirali7862105@gmail.com>
2023-02-07 20:13:50 +0530

    s

---
+++ LINUX/RH124/video-list.js
@@ -0,0 +1,33 @@
+let allVideos = [
+   {
+      name: "Welcome to RH124 Module.mp4",
+      src: "uc?id=1DH-anGhWEfZrp5_iBTjMb9xJymPdrZ4M",
+      id: "vid_1"
+   },
+  
+       {
+      name: "Get started with Red Hat Enterprise Linux.mp4",
+      src: "uc?id=1_32JySTxMNGXkmzQuGLNZHEXlsYn0BMJ",
+      id: "vid_4"
+   },
+     {
+      name: "Access the command line.mp4",
+      src: "uc?id=1ivyF-fXBk8mgTdF4VuCMvcMIUpGRk7V6",
+      id: "vid_3"
+   },
+   {
+      name: "Manage files from command line.mp4",
+      src: "uc?id=1lgRB21JpCRhFPes1o5H05yzBeQyuvPuF",
+      id: "vid_3"
+   },
+   {
+      name: "Get help in Red Hat Enterprise Linux.mp4",
+      src: "uc?id=1auOBhc88tRPNto8OZaM5vuylEhiJgvd_",
+      id: "vid_3"
+   }
+   
+
+   ]
+   
+   
+   
