d6520c32b728813175b945dcb690e10ad3543491
tahiraliprofile <tahirali7862105@gmail.com>
2023-02-07 17:24:57 +0530

    j

---
+++ NETWORKING/1/video-list.js
@@ -0,0 +1,53 @@
+let allVideos = [
+   {
+      name: "Welcome.mp4",
+      src: "uc?id=1v5bhptDQwFOG1iXLtmEBCDN7Yl3Cjus-",
+      id: "vid_1"
+   },
+  
+   {
+      name: "Button Hover Effect",
+      src: "Button Hover Effect Using Html And CSS",
+      id: "vid_2"
+   },
+   {
+      name: "Confirm Password using Html CSS & js",
+      src: "Confirm Password using Html CSS & js",
+      id: "vid_3"
+   },
+   {
+      name: "Creative Card Hover Effect",
+      src: "Creative Card Hover Effect ",
+      id: "vid_4"
+   },
+   {
+      name: "Creative Digital Clock Ui Design",
+      src: "Creative Digital Clock Ui Design - Html Css & Vanilla Javascript-1",
+      id: "vid_5"
+   },
+   {
+      name: "Creative Our Team Section",
+      src: "Creative Our Team Section Using HTML & CSS Neumorphism",
+      id: "vid_6"
+   },
+   {
+      name: "Filter Text Animation Using Html and CSS",
+      src: "Filter Text Animation Using Htm and Pure CSS",
+      id: "vid_7"
+   },
+   {
+      name: "Glassmorphism Calculater New Design",
+      src: "Glassmorphism Calculater New Design Html",
+      id: "vid_8"
+   },
+   {
+      name: "How To  Make Cheatsheet",
+      src: "How To  Make Cheatsheet Using Html, CSS and Javascript",
+      id: "vid_9"
+   },
+   {
+      name: "How to create Login Form",
+      src: "How to create Login Form Using Html and CSS",
+      id: "vid_10"
+   }
+]
